f81ed44e06901d717d9017ed616e6c31fa8f8742
Semis <lovekmi11@gmail.com>
2020-09-22 17:11:47 +0800

    Mid Autumn

---
+++ js/photo.js
@@ -0,0 +1,5 @@
+/*2019*/
+function mouseOver() {document.getElementById('E60').src = "images/photo/Formulaphoto_E60_eyeshadow_Neon Shadow-01.jpg"};
+function mouseOut() {document.getElementById('E60').src = "images/formula/E60_eyeshadow_Neon%20Shadow-01.jpg"};
+function mouseOver() {document.getElementById('E59').src ="images/photo/Formulaphoto_E59_Shadow Liner-01.jpg"};
+function mouseOut() {document.getElementById('E59').src = "images/formula/E59_eyeshadow_Shadow%20Liner-01.jpg"};
